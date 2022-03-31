ca04ab19ab0945c5c7af0ceb0b2fe7013962e1c8
achourali <aliachour@insat.u-carthage.tn>
2022-03-31 12:17:31 +0100

    initial commit : flex analyser

---
+++ javaFiles/example1.java
@@ -0,0 +1,7 @@
+public class example1 {
+    public void test() {
+        int x = 0;
+        if (x == 1)
+            System.out.println(x);
+    }
+}
---
+++ runFlex.sh
@@ -0,0 +1,5 @@
+
+# arguments:
+#     $1 : path to the java file  
+
+flex -o ./build/lex.yy.c ./src/miniJavaCompiler.lex  && gcc -o ./build/a.out ./build/lex.yy.c  && ./build/a.out $1
